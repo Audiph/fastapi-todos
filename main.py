4806b7f72c1467b8719963a53ab6aed6bc410584
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-03 14:47:22 +0800

    implement create todo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel, Field
 from starlette import status
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -23,6 +24,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+  title: str = Field(min_length=3)
+  description: str = Field(min_length=3, max_length=100)
+  priority: int = Field(gt=0, lt=6)
+  complete: bool
+
+
 @app.get('/', status_code=status.HTTP_200_OK)
 async def get_todos(db: db_dependency):
   return db.query(Todos).all()
@@ -36,3 +44,11 @@ async def get_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
     return todo_model
 
   raise HTTPException(status_code=404, detail='Todo not found.')
+
+
+@app.post('/todo', status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+  todo_model = Todos(**todo_request.model_dump())
+
+  db.add(todo_model)
+  db.commit()
